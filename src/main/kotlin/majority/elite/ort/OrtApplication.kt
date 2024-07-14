272498cbb8c63b5eb74e0e3e026d5caba054aa5e
Godbell <12191579@inha.edu>
2024-07-14 16:25:33 +0900

    initial

---
+++ src/main/kotlin/majority/elite/ort/OrtApplication.kt
@@ -0,0 +1,11 @@
+package majority.elite.ort
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrtApplication
+
+fun main(args: Array<String>) {
+	runApplication<OrtApplication>(*args)
+}
---
+++ src/test/kotlin/majority/elite/ort/OrtApplicationTests.kt
@@ -0,0 +1,13 @@
+package majority.elite.ort
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OrtApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
