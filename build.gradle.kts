741f3530457d13affab622cdf381ffcae97169c0
Godbell <12191579@inha.edu>
2024-07-14 18:25:04 +0900

    Chore: adjust jvm version to 22

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
 	id("org.springframework.boot") version "3.3.1"
 	id("io.spring.dependency-management") version "1.1.5"
-	kotlin("jvm") version "1.9.24"
+	kotlin("jvm") version "2.0.0"
 	kotlin("plugin.spring") version "1.9.24"
 }
 
@@ -10,7 +10,7 @@ version = "0.0.1-SNAPSHOT"
 
 java {
 	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
+		languageVersion = JavaLanguageVersion.of(22)
 	}
 }
 
