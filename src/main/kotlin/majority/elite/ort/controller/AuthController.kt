a5c678c1cb1b46943ca1ae889a7dec4393eafbb1
Godbell <12191579@inha.edu>
2024-08-27 15:06:09 +0900

    Feat: change signout api path to /logout

--- src/main/kotlin/majority/elite/ort/controller/AuthController.kt
+++ src/main/kotlin/majority/elite/ort/controller/AuthController.kt
@@ -8,7 +8,6 @@ import majority.elite.ort.exception.TokenExpiredException
 import majority.elite.ort.exception.UnauthorizedException
 import majority.elite.ort.service.OAuth2UserService
 import majority.elite.ort.service.OrtJwtService
-import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -37,7 +36,7 @@ class AuthController(
     )
   }
 
-  @GetMapping("/signout")
+  @GetMapping("/logout")
   fun signOut(@RequestHeader("Authorization") bearerToken: String): String {
     val accessToken = bearerToken.split(" ")[1]
     val userId = (ortJwtService.verifyToken(accessToken)["userId"] as String).toLong()
