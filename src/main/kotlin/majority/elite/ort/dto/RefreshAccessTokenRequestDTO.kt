6067a768dc5b1457c9118d0776d64392f31dfd0a
Godbell <12191579@inha.edu>
2024-09-05 03:51:45 +0900

    [ORT-6] Feat: add openapi v3.0 springdoc (swagger supported) (#2)
    TODO: add validation to DTO
    
    * Feat: attach springdoc openapi 3.0
    
    * Refactor: remove redundant import
    
    * Doc: add todo

--- src/main/kotlin/majority/elite/ort/dto/RefreshAccessTokenRequestDTO.kt
+++ src/main/kotlin/majority/elite/ort/dto/RefreshAccessTokenRequestDTO.kt
@@ -1,9 +1,9 @@
 package majority.elite.ort.dto
 
-import javax.validation.constraints.NotBlank
 import lombok.NoArgsConstructor
 
 @NoArgsConstructor
 class RefreshAccessTokenRequestDTO() {
-  @NotBlank(message = "No refresh token provided") val refreshToken: String = ""
+  // TODO("add validation")
+  val refreshToken: String = ""
 }
